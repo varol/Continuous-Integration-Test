94f3eee75ba1c7b289313e10ca58e42150c32d94
Varol AKSOY <myturkey@gmail.com>
2019-10-29 12:32:02 +0300

    Initial Commit

---
+++ Continuous Integration Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Continuous Integration Test
+//
+//  Created by VAROL AKSOY on 29.10.2019.
+//  Copyright © 2019 VAROL AKSOY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
